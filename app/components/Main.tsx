3d91b9740a132a66e7d4581516eb0008e913fedf
unknown <scamfreeind@gmail.com>
2026-04-07 17:43:50 +0530

    feat: implement core UI components and static pages for scam awareness platform

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -218,7 +218,7 @@ export default function Main() {
 
             <FraudSection />
             <SebiIntermediaries />
-            {/* <Expert /> */}
+            <Expert />
             <Review />
             <Video />
             <Awareness />
